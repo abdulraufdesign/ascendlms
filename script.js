9e6fa5796e4ea56e6476be56384350434d6e41d2
Abdul Rauf <177952790+abdulraufdesign@users.noreply.github.com>
2025-04-28 01:33:57 +0000

    ascend page done

--- script.js
+++ script.js
@@ -3,7 +3,4 @@ let themeCta = document.querySelector(".theme-cta");
 themeCta.addEventListener("click", () => {
   document.documentElement.classList.toggle("second");
   themeCta.classList.toggle("dark");
-  setTimeout(() => {
-    changeDepart();
-  }, 50);
 });
